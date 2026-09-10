44aae631d0feae3cf046a8f1d68d7cfcd3a8f849
xingruliming <3181924945@qq.com>
2026-09-10 09:43:22 +0800

    删除不要的测试软件

--- src-springboot/main/java/com/example/demo/mapper/AnnouncementMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.demo.entity.Announcement;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface AnnouncementMapper extends BaseMapper<Announcement> {
-}
--- src-vue/main/java/com/example/demo/mapper/InviteMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.demo.entity.Invite;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface InviteMapper extends BaseMapper<Invite> {
-}
